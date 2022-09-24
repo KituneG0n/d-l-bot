d407678f18796bd4420e28999fa79afa9012ade9
KituneG0n <89195137+KituneG0n@users.noreply.github.com>
2022-09-24 22:06:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,51 @@
-// Response for Uptime Robot
-const http = require("http");
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("DiscordBot is active Please don't close this page \n");
-  })
-  .listen(3000);
+const line = require('@line/bot-sdk');
+const discord = require('discord.js');
 
-// Discord bot implements
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const lineConfig = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+const lineClient = new line.Client(lineConfig);
 
-client.once("ready", () => {
-  // botのステータス表示
-  client.user.setActivity("LINE");
-  console.log("bot is ready!");
-});
+const discordClient = new discord.Client();
 
-client.on("message", (message) => {
-  // bot(自分)のメッセージには反応しない(これをしないと兵庫県警に捕まる)
-  if (message.author.bot) {
-    return;
+const generateMessage = (event) => {
+  const content = event.content;
+  if (content.length === 0) {
+    return {
+      type: 'text',
+      text: 'Discordで非対応の形式のメッセージを送信しました。',
+      sender: {
+        name: event.author.username,
+        iconUrl: event.author.displayAvatarURL().replace('.webp', '.png'),
+      },
+    };
   }
-  // DMには応答しない
-  if (message.channel.type == "dm") {
-    return;
-  }
+  return {
+    type: 'text',
+    text: `${content}`,
+    sender: {
+      name: event.author.username,
+      iconUrl: event.author.displayAvatarURL().replace('.webp', '.png'),
+    },
+  };
+};
 
-  const msg = message;
-
-  // botへのリプライは無視
-  if (msg.mentions.has(client.user)) {
-    return;
-  } else {
-    // GASにメッセージを送信
-    if (msg.channel.id == process.env.CHANNEL_ID) {
-      sendGAS(msg);
+discordClient.on('message', async (event) => {
+  try {
+    // BOTのメッセージはスルー
+    if (!event.author.bot) {
+      const message = generateMessage(event);
+      await lineClient.pushMessage(
+        // 送信先のトークルームのグループ/ユーザーID
+        process.env.LINE_TARGET_ID,
+        message,
+      );
     }
-
-    return;
-  }
-
-  function sendGAS(data) {
-    // LINE Messaging API風の形式に仕立てる(GASでの場合分けが楽になるように)
-    const jsonData = {
-      events: [
-        {
-          type: "discord",
-          name: data.author.username,
-          message: data.content,
-        },
-      ],
-    };
-    // GAS URLに送る
-    post(process.env.GAS_URL, jsonData);
-  }
-
-  function post(url, data) {
-    // requestモジュールを使う
-    const request = require("request");
-    const options = {
-      uri: url,
-      headers: { "Content-type": "application/json" },
-      json: data,
-      followAllRedirects: true,
-    };
-    // postする
-    request.post(options, function (error, response, body) {
-      if (error != null) {
-        msg.reply("更新に失敗しました");
-        return;
-      }
-
-      const userid = response.body.userid;
-      const channelid = response.body.channelid;
-      const text = response.body.message;
-      if (userid != undefined && channelid != undefined && text != undefined) {
-        const channel = client.channels.get(channelid);
-        if (channel != null) {
-          channel.send(text);
-        }
-      }
-    });
-  }
+  } catch(error) {
+    console.error(error);
+  } 
 });
 
-if (process.env.DISCORD_BOT_TOKEN == undefined) {
-  console.log("please set ENV: DISCORD_BOT_TOKEN");
-  process.exit(0);
-}
+discordClient.login(process.env.DISCORD_ACCESS_TOKEN);
 
-client.login(process.env.DISCORD_BOT_TOKEN);
